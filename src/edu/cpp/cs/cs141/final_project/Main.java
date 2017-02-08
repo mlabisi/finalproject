dcce377107efc62e58b44920d97c37db1fd5b7a7
Mora <mlabisi98@gmail.com>
2017-02-08 09:28:04 -0800

    Update Main.java

--- src/edu/cpp/cs/cs141/final_project/Main.java
+++ src/edu/cpp/cs/cs141/final_project/Main.java
@@ -1,5 +1,17 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Final Project: Spy Game
+ *
+ * Create a small, yet interesting, text-based game involving a spy
+ * and ninjas.
+ *
+ * Team Magic!~<3
+ * Diana Choi, William Hang, Logan Carichner, Robert Delfin, Mora Labisi
+ */
 public class Main{
   public static void main(String[] args){
-  
+    // implement main and engine
   }
 }

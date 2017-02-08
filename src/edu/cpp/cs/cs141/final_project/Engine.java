610a260ffae02dd9df3cd008707ff1d8f45af2a7
robertdelfin <chuppachuppa99@yahoo.com>
2017-02-08 09:23:50 -0800

    Add files via upload

---
+++ src/edu/cpp/cs/cs141/final_project/Engine.java
@@ -0,0 +1,63 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment Final Project
+ *
+ * To make a game that has a spy that has
+ * to find a briefcase in a building full
+ * of ninjas.
+ *
+ * Team Magic!~<3
+ *   Robert Delfin
+ *   William Hang
+ *   Diana Choi
+ *   Logan Carichner
+ *   Mora Labisi
+ */
+
+package edu.cpp.cd.cs141.final_project;
+
+/**
+ * @author Robert Delfin
+ *
+ */
+public class Engine {
+	private int turns;
+	private int lives;
+	private int enemies;
+	private int ammo;
+	private boolean end;
+	
+	/**
+	 * This method loops the game until
+	 * the victory condition is set and 
+	 * the player wins.
+	 */
+	public void LoopGame(){
+		
+	}
+	
+	/**
+	 * This method increase the amount
+	 * of turns and tracks the amount.
+	 * @return the turn count.
+	 */
+	public int addTurn(){
+	   return 0;	
+	}
+	public void countEnemy(){
+		
+	}
+	
+	/**
+	 * This method changes the game to the
+	 * victory condition.
+	 * @return the statement to end the game.
+	 */
+	public boolean endGame(){
+		return false;
+	}
+	
+
+}

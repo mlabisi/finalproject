8c9118f52885c3d2da90c6e008740829a2efd874
dianajchoi <dianajchoi@gmail.com>
2017-02-10 14:07:07 -0800

    added description and setBoard method

--- src/edu/cpp/cs/cs141/final_project/Diana/Board.java
+++ src/edu/cpp/cs/cs141/final_project/Diana/Board.java
@@ -11,8 +11,11 @@
  */
 package edu.cpp.cs.cs141.final_project.Diana;
 
+import edu.cpp.cs.cs141.final_project.*;
+
 /**
- * This class represents
+ * This class represents the Board which holds a 9x9 grid of {@link Square},
+ * and its subclasses {@link Hallway} and {@link Room}.
  * 
  * @author Diana Choi
  *
@@ -22,8 +25,32 @@ public class Board {
 	/**
 	 * 
 	 */
+	private Square[][] grid;
+	
+	/**
+	 * 
+	 */
+	public static final int BOARD_SIZE = 9;
+	
+	
+	/**
+	 * 
+	 */
 	public Board() {
-		// TODO Auto-generated constructor stub
+		grid = new Square[BOARD_SIZE][BOARD_SIZE];
 	}
+	
+	public void setBoard(){
+		for(int i = 0; i < BOARD_SIZE; ++i){
+			for( int j = 0; j < BOARD_SIZE; ++j){
+				if(i%3 == 1 && j%3 == 1){
+					grid[i][j] = new Room();
+				}else{
+					grid[i][j] = new Hallway();
+				}
+			}
+		}
+	}
+	
 
 }

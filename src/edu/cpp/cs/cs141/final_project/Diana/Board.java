79cd9df76cdff53a2c1c537c9ddfdc4bc8c196ed
dianajchoi <dianajchoi@gmail.com>
2017-02-10 13:38:46 -0800

    trying to figure out how to import the repo

---
+++ src/edu/cpp/cs/cs141/final_project/Diana/Board.java
@@ -0,0 +1,29 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment #N
+ *
+ * <description-of-assignment>
+ *
+ * Team #N / (or name if individual)
+ *   <team-member-names-if-team-assignment>
+ */
+package edu.cpp.cs.cs141.final_project.Diana;
+
+/**
+ * This class represents
+ * 
+ * @author Diana Choi
+ *
+ */
+public class Board {
+
+	/**
+	 * 
+	 */
+	public Board() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

72e68420c70a0dc4567189b1d864da0c42870c6e
Mora Labisi <mlabisi98@gmail.com>
2017-02-12 16:01:08 -0800

    Added ItemType and BoardDemo

---
+++ src/edu/cpp/cs/cs141/final_project/BoardDemo.java
@@ -0,0 +1,11 @@
+package edu.cpp.cs.cs141.final_project;
+
+/**
+ * @author Mora Labisi
+ */
+public class BoardDemo {
+    public static void main(String[] args){
+        Board gameBoard = new Board();
+        System.out.println(gameBoard);
+    }
+}

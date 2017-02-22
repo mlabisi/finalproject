5da7f794b1252a5e182675ea076a43f1bf59e306
LoganCarichner <ldcarichner@gmail.com>
2017-02-22 09:34:00 -0800

    Add files via upload

---
+++ Engine.java
@@ -0,0 +1,85 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment Final Project
+ *
+ * To make a game that has a spy that has
+ * to find a briefcase in a building full
+ * of ninjas.
+ *
+ * Team Magic!~<3
+ *   Robert Delfin
+ *   William Hang
+ *   Diana Choi
+ *   Logan Carichner
+ *   Mora Labisi
+ */
+
+package edu.cpp.cs.cs141.final_project;
+
+import java.io.Serializable;
+
+/**
+ * @author Robert Delfin
+ * @author Logan Carichner
+ *
+ */
+public class Engine implements Serializable {
+	private Board board;
+	private boolean debug;
+	private int turns;
+	private int lives;
+	private int enemies;
+	private int boardSize;
+	private boolean win = false;
+	
+	public Engine() {
+		boardSize = UI.getBoardSize();
+		debug = UI.debugStart();
+		board = new Board(boardSize, debug);
+		turns = 0;
+	}
+	
+	public void printBoard() {
+		board.printBoard();
+	}
+	
+	public static int roll(int num) {
+		int generated = (int) Math.floor(Math.random() * (num));
+		return generated;
+	}
+
+	public void enemyTurn() {
+		board.moveNinjas();
+		turns++;
+	}
+	
+	public String getDebug() {
+		if (debug)
+			return "ON";
+		return "OFF";
+	}
+	
+	public void toggleDebug() {
+		debug = !debug;
+		board.debugRooms();
+	}
+
+	public void killNinja() {
+		board.killNinja();
+	}
+
+	public int getNumRooms() {
+		return board.getNumRooms();
+	}
+	
+	public boolean getState() {
+		return win;
+	}
+	
+	public void quit() {
+		System.exit(0);
+	}
+
+}
---
+++ Weapon.java
@@ -0,0 +1,52 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Final Project: Spy Game
+ *
+ * Create a small, yet interesting, text-based game involving a spy
+ * and ninjas.
+ *
+ * Team Magic!~<3
+ * Diana Choi, William Hang, Logan Carichner, Robert Delfin, Mora Labisi
+ */
+package edu.cpp.cs.cs141.final_project;
+
+/**
+ * This class represents the weapons in the game which includes the
+ * attributes and behaviors of the weapons. The agent will have a pistol, 
+ * and the ninja-assassin will have a knife.
+ *
+ * @author Mora Labisi
+ */
+public class Weapon {
+	/**
+	 * This is the constructor for the weapon. 
+	 */
+	public Weapon(){
+	}
+	/**
+	 * 	This represents whether or not the 
+	 * agent has ammo in the gun.
+	 */
+	private boolean hasAmmo;
+	/**
+	 *  This represents the maximum ammunition
+	 *  the agent can have
+	 */
+	private int maxAmmo;
+	/**
+	 * This method will kill the ninja-assassin if the 
+	 * bullet hits the ninja-assassin.
+	 */
+	public void killEnemy(){
+		
+	}
+	/**
+	 * This represents the action of the ninja-assassin
+	 * attacking the agent.
+	 */
+	public int stabAgent(int agentHealth){
+       		return agentHealth;
+	}
+}
